43d6808a5d32d56da112a5cc8883057fd4b3ef53
danlukic01 <141298464+danlukic01@users.noreply.github.com>
2025-06-16 11:54:40 +1000

    Add UI for contact requests

--- src/MatchingApp.Api/Data/AppDbContext.cs
+++ src/MatchingApp.Api/Data/AppDbContext.cs
@@ -10,6 +10,7 @@ namespace MatchingApp.Api.Data
         public DbSet<Client> Clients => Set<Client>();
         public DbSet<NatalChart> NatalCharts => Set<NatalChart>();
         public DbSet<Match> Matches => Set<Match>();
+        public DbSet<ContactRequest> ContactRequests => Set<ContactRequest>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -17,6 +18,9 @@ namespace MatchingApp.Api.Data
             modelBuilder.Entity<Match>()
                 .HasIndex(m => new { m.ClientAId, m.ClientBId })
                 .IsUnique();
+
+            modelBuilder.Entity<ContactRequest>()
+                .HasIndex(c => new { c.SenderId, c.ReceiverId });
 
             // No need to define the index for Client.Email again,
             // because the [Index] attribute on Client takes care of it
